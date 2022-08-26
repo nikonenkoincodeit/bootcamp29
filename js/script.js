28a829b5e5fc68f53bd033a3471d2964cd33c0d6
nikonenkoincodeit <75611895+nikonenkoincodeit@users.noreply.github.com>
2022-08-26 15:20:56 +0300

    result

---
+++ js/script.js
@@ -0,0 +1,102 @@
+// todo Example 8 - Логічні оператори
+
+// console.log(true && 3 && 2);
+
+// console.log(false && 3);
+
+// console.log(true && 4 && "kiwi");
+
+// console.log(true && 0 && "kiwi");
+
+// console.log(true || 3);
+
+// console.log(true || 3 || 4);
+
+// console.log(true || false || 7);
+
+// console.log(null || 2 || undefined);
+
+// console.log((1 && null && 2) > 0);
+
+// console.log(null || (2 && 3) || 4);
+
+// console.log(Boolean(undefined));
+// console.log(!!undefined);
+// console.log(undefined);
+
+const test = "";
+
+// const flag = !!test;
+
+// if (Boolean(undefined)) {
+//   console.log("1111 :>> ", 1111);
+// }
+
+// if (!!undefined) {
+//   console.log("1111 :>> ", 1111);
+// }
+
+// if (undefined) {
+//   console.log("1111 :>> ", 1111);
+// }
+
+// console.log(5 === 5);
+
+// todo Оператори порівняння та приведення типів
+
+// console.log(5 > 5);
+
+//console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean(null));
+//console.log(Boolean(''));
+// console.log(Boolean(NaN));
+// console.log(Boolean(false));
+// console.log(Boolean(-0));
+
+// console.log(Boolean([]));
+// console.log(Boolean({}));
+// console.log(Boolean(function () {}));
+
+// console.log(10 >= "7");
+
+// console.log("A" === "a");
+
+// console.log("A".charCodeAt(0));
+// console.log("a".charCodeAt(0));
+
+// console.log("231" >= "23");
+// console.log(1 > false);
+//console.log("2".charCodeAt(0));
+// console.log("1".charCodeAt(0));
+
+// console.log("4" == 4);
+
+// console.log("6" === 6);
+// console.log(NaN == NaN);
+
+// console.log("false" === false);
+
+// console.log(1 == true);
+
+// console.log(1 === true);
+
+// console.log("0" == false);
+
+// console.log("0" === false);
+
+// console.log("Papaya" < "papaya");
+// console.log("P".charCodeAt(0));
+// console.log("p".charCodeAt(0));
+
+// console.log("Papaya" === "papaya");
+
+// console.log(undefined == null);
+
+// console.log(1 && null && 2);
+// alert(alert(1) && alert(2));
+//alert(null || (2 && 3) || 4);
+
+if (-1 || 0) alert("first");
+if (-1 && 0) alert("second");
+if (null || (-1 && 1)) alert("third");
